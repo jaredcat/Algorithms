95c6edc1d64f205af5c034d28ff1240efb2e2187
Jared Kotoff <krylic@gmail.com>
2014-10-05 23:09:42 -0700

    Near completion of project 2

--- Project 2/project2.py
+++ Project 2/project2.py
@@ -19,38 +19,65 @@ class DebianPackage(object):
         return self.size
 
     def __str__(self):
-        return self.name + " " + self.votes + " " + self.size
+        return self.name + " " + str(self.size) + " " + str(self.votes)
 
 
 def read_packages(file, n):
+    first = 1
+    i = 0
     with open(file, "r") as f:
-        pkg_list = []
-        for line in f[1:n]:
-            line = line.split(" ")
-            new_pkg = DebianPackage(line[0], int(line[1]), int(line[2]))
-            pkg_list.append(copy.deepcopy(new_pkg))
+            pkg_list = []
+            for line in f:
+                if not first:
+                    if i == n:
+                        break
+                    line = line.split(" ")
+                    new_pkg = DebianPackage(line[0], int(line[1]), int(line[2]))
+                    pkg_list.append(copy.deepcopy(new_pkg))
+                    i += 1
+                else:
+                    first = 0
+                    i += 1
     return pkg_list
 
 
 def subsets(package_list):
     result = [[]]
-
+    for x in package_list:
+        with_x = []
+        for subset in result:
+            with_x.append(subset + [x])
+        result = result + with_x
     return result
 
 
 def verify_knapsack(candiate, W):
-
-    return 0
+    sum = 0
+    for x in candiate:
+        sum += x.size
+    if sum > W:
+        return 0
+    else:
+        return 1
 
 
 def compare_knapsack(candidate, best):
-    return 0
+    candidate_votes = 0
+    best_votes = 0
+    for x in candidate:
+        candidate_votes += x.votes
+    for x in best:
+        best_votes += x.votes
+    if candidate_votes <= best_votes:
+        return 0
+    else:
+        return 1
 
 
 def exhaustive_knapsack(package_list, W):
-    best = None
+    best = []
     for candidate in subsets(package_list):
-        if verify_knapsack(candiate, W):
+        if verify_knapsack(candidate, W):
             if compare_knapsack(candidate, best):
                 best = candidate
     return best
@@ -66,20 +93,20 @@ def main():
     n = int(sys.argv[2])+1
     W = int(sys.argv[3])'''
     filename = "packages.txt"
-    n = 23
-    W = 2000
+    n = 20+1
+    W = 1000
 
     pkg_list = read_packages(filename, n)
 
-    print(pkg_list)
-
-    '''start = time.perf_counter()
+    start = time.perf_counter()
     f = exhaustive_knapsack(pkg_list, W)
     end = time.perf_counter()
-    print('------ n=' + n + ' W=' + W)
+    print('------ n=' + str(n-1) + ' W=' + str(W))
     print('    ---Exhaustive search solution ---')
-    print(f)
-    print('       Elapsed time = ' + str(end - start) + "seconds")'''
+    test = ""
+    for i in f:
+        print('       ' + str(i))
+    print('       Elapsed time = ' + str(end - start) + " seconds")
 
 if __name__ == '__main__':
     main()
